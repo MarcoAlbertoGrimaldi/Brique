d24320fe7a251113b5668b71cbba7f2fa0dc2d05
MarcoAlbertoGrimaldi <marco.alberto.grimaldi@gmail.com>
2020-06-11 20:38:45 +0200

    working

--- src/main/java/CommandLine/PlayerInputHandler.java
+++ src/main/java/CommandLine/PlayerInputHandler.java
@@ -3,15 +3,15 @@ package CommandLine;
 public class PlayerInputHandler implements InputInterface {
 
     final String player_request_msg = "Type you nickname:";
-    final String player_err_msg = "Unexpected error";
+    final String player_err_msg = "This nickname is not valid. Try another one:";
     final String player_pattern = "\\w+";
 
     final String pie_rule_request_msg = "If you want to apply the pie rule, type '1'; otherwise type '0'.";
-    final String pie_rule_err_msg = "Please type '0' or '1'.";
+    final String pie_rule_err_msg = "Not valid. Please type '0' or '1':";
     final String pie_rule_pattern = "[0-1]";
 
-    final String coordinate_request_msg = "Insert coordinates or type 'res' to resign:";
-    final String coordinate_err_msg = "Not valid, please insert again (a-o and 1-15, e.g. f3)";
+    String coordinate_request_msg = "Insert valid coordinates or type 'res' to resign:";
+    String coordinate_err_msg = "Not valid. Please insert again (a-o and 1-15, e.g. f3) or type 'res' to resign:";
     final String coordinate_pattern = "[a-o][1-9]|[a-o]1[0-5]|res";
 
     public String getPlayer_request_msg() {
