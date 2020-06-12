c191c033c158be5989d29d24222479eed09d0ab5
apresta <presta.alberto@libero.it>
2020-06-12 11:10:07 +0200

    Refactoring_of_tests

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,5 +1,6 @@
 
 
+import Data.Graph;
 import Data.PieceColor;
 import Data.Player;
 import org.junit.Test;
@@ -19,6 +20,15 @@ public class PlayerTest {
         Player player = new Player(PieceColor.WHITE,"player_1");
         Assert.assertTrue(player.isHuman());
         Assert.assertEquals(player.getName(),"player_1");
+    }
+
+    @Test
+    public void check_swapGraphs(){
+        Player player1 = new Player(PieceColor.BLACK, "Human");
+        Player player2 = new Player(PieceColor.WHITE, "Human_2");
+        Graph player1_graph = player1.getGraph();
+        player1.swapGraphs(player2);
+        Assert.assertEquals(player1_graph,player2.getGraph());
     }
 
 
