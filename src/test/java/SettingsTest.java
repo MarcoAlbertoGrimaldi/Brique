5b364e8677e61ea4eae34d707e5fd64ec6150d85
apresta <presta.alberto@libero.it>
2020-06-12 10:31:35 +0200

    SettingsTest

---
+++ src/test/java/SettingsTest.java
@@ -0,0 +1,27 @@
+
+
+import Data.Settings;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.awt.*;
+
+
+public class SettingsTest {
+
+    @Test
+    public void getResolution() {
+        Assert.assertEquals(Settings.getResolution(), new Dimension(720, 720));
+    }
+
+    @Test
+    public void setResolution() {
+        Settings.setResolution(0);
+        Assert.assertEquals(Settings.getResolution(), new Dimension(480, 480));
+    }
+
+    @Test
+    public void toDimension() {
+        Assert.assertEquals(new Dimension(720, 720), Settings.toDimension("720x720"));
+    }
+}
